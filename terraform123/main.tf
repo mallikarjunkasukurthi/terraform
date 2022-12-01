7eeed93653e34f0c88219d95b0fac741cef24e04
mallikarjunarao <mallikarjun.kasu1987@gmail.com>
2022-12-01 18:27:27 +0530

    s3'

--- terraform123/main.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-  region = "ap-south-1"
-  access_key = "AKIASBDEDVLNHJ3OSCPM"
-  secret_key = "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"
-}
-resource "aws_s3_bucket" "mys3bucket" {
-  bucket = "mys3bucket@138790"
-}
